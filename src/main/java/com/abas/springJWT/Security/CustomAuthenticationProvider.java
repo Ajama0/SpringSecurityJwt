fe61705a1f2dc311ceb15132cd50b3b097b84ffe
Ajama0 <abasjama04@hotmail.com>
2025-01-04 22:13:04 +0000

    allow the authentication provider to handle any object

--- src/main/java/com/abas/springJWT/Security/CustomAuthenticationProvider.java
+++ src/main/java/com/abas/springJWT/Security/CustomAuthenticationProvider.java
@@ -10,14 +10,16 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
      * the jwt only validates the token, and checks that the subject exists in the database
      * the rest is handled by the authentication provider
      * setting supports to true allows jwtfilter to hand authentication over to the auth provider
+     * this is because our custom auth provider will be used
      */
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
+        //means our authentication provider can handle any type of authentication object
         return true;
     }
 }
