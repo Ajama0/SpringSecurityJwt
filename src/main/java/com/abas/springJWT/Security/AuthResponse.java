a7f53cbe44465c236e839a19cfa3617cc1cbd654
Ajama0 <abasjama04@hotmail.com>
2025-01-05 15:40:14 +0000

    add custom response within controllers

---
+++ src/main/java/com/abas/springJWT/Security/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.abas.springJWT.Security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+
+    private String token;
+}
--- src/main/java/com/abas/springJWT/Security/Controller/Authentication.java
+++ src/main/java/com/abas/springJWT/Security/Controller/Authentication.java
@@ -1,4 +1,36 @@
 package com.abas.springJWT.Security.Controller;
 
+
+import com.abas.springJWT.Security.AuthResponse;
+import com.abas.springJWT.Security.AuthenticationRequest;
+import com.abas.springJWT.Security.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/Auth")
 public class Authentication {
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+
+        return new ResponseEntity<>(new AuthResponse(token),HttpStatus.CREATED);
+
+    }
+
+
+
+
+    @PostMapping(path = "/authenticate")
+    public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthenticationRequest request){
+
+
+
+    }
 }
